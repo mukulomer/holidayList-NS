14763bd216320e58555eb8669d960c50d9488a67
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-06 16:00:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+
+// const Cities = [
+//   "Goa(India)",
+//   "Amsterdam(Netherlands)",
+//   "New York(USA)",
+//   "Darjeeling(India)",
+//   "Tokyo(Japan)",
+//   "Lonavala(India)"
+// ];
+// const getCity = Cities.map((city) => city);
+export default function App() {
+  return (
+    <div className="App">
+      <h1>List Of cities</h1>
+      <ol>
+        <li key={"location1"}>Goa(India) </li>
+        <li key={"location2"}>Amsterdam(Netherlands) </li>
+        <li key={"location3"}>New York(USA)</li>
+        <li key={"location4"}>Darjeeling(India)</li>
+        <li key={"location5"}>Tokyo(Japan)</li>
+        <li key={"location6"}>Lonavala(India)</li>
+      </ol>
+    </div>
+  );
+}
